1388475f50550040c5ec89860608951cb5766e64
MMAugusto <am9228357@gmail.com>
2024-04-24 20:48:55 +0100

    correcting errors

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("Análise de Dados de Veículos")
 st.header("Este é um aplicativo para análise de dados de veículos.")
 
 # Carregando os dados
-@st.cache
+@st.cache_data
 def load_data():
     data = pd.read_csv("vehicles.csv")
     return data
@@ -22,23 +22,23 @@ opcao_grafico = st.radio("Selecione o tipo de gráfico:", ("Histograma", "Gráfi
 if opcao_grafico == "Histograma":
     # Criando um histograma
     st.subheader("Histograma da Distribuição de Preços dos Veículos")
-    plt.figure(figsize=(8, 6))
-    sns.histplot(data['price'], bins=20, kde=True)
-    plt.xlabel('Preço')
-    plt.ylabel('Contagem')
-    plt.title('Distribuição de Preços')
-    st.pyplot()
+    fig, ax = plt.subplots()
+    sns.histplot(data['price'], bins=20, kde=True, ax=ax)
+    ax.set_xlabel('Preço')
+    ax.set_ylabel('Contagem')
+    ax.set_title('Distribuição de Preços')
+    st.pyplot(fig)
 
 elif opcao_grafico == "Gráfico de Dispersão":
     # Criando um gráfico de dispersão
     st.subheader("Gráfico de Dispersão: Preço vs. Ano do Modelo")
-    plt.figure(figsize=(8, 6))
-    sns.scatterplot(data=data, x='model_year', y='price', hue='condition')
-    plt.xlabel('Ano do Modelo')
-    plt.ylabel('Preço')
-    plt.title('Preço vs. Ano do Modelo')
-    plt.legend(title='Condição')
-    st.pyplot()
+    fig, ax = plt.subplots()
+    sns.scatterplot(data=data, x='model_year', y='price', hue='condition', ax=ax)
+    ax.set_xlabel('Ano do Modelo')
+    ax.set_ylabel('Preço')
+    ax.set_title('Preço vs. Ano do Modelo')
+    ax.legend(title='Condição')
+    st.pyplot(fig)
 
 # Adicionando um botão para exportar os dados
 if st.button("Exportar Dados"):
